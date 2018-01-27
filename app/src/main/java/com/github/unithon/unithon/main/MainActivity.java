ecd41980799e625d64e172ae3941d50a12deabbf
Jaehwan <ojh102@gmail.com>
2018-01-27 10:59:46 +0900

    adding BottomNavigation, SearchActivity

--- app/src/main/java/com/github/unithon/unithon/main/MainActivity.java
+++ app/src/main/java/com/github/unithon/unithon/main/MainActivity.java
@@ -1,14 +1,49 @@
 package com.github.unithon.unithon.main;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import com.github.unithon.unithon.R;
+import com.github.unithon.unithon.search.SearchActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    @BindView(R.id.navigation)
+    BottomNavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        navigationView.setOnNavigationItemSelectedListener(this);
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch(item.getItemId()) {
+            case R.id.navigation_home:
+
+
+                return true;
+            case R.id.navigation_search:
+                final Intent intent = new Intent(this, SearchActivity.class);
+                startActivity(intent);
+
+                return true;
+            case R.id.navigation_mypage:
+
+                return true;
+            default:
+                return false;
+        }
+
+    }
+
 }
---
+++ app/src/main/java/com/github/unithon/unithon/search/SearchActivity.java
@@ -0,0 +1,14 @@
+package com.github.unithon.unithon.search;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.github.unithon.unithon.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
